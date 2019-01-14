aa425b80b61a33265a8f68c90428a704633f3f70
qshuai <qishuai231@gmail.com>
2019-01-15 00:47:30 +0800

    Ensure print sequence using a common channel

--- golang/sequence_print_test.go
+++ golang/sequence_print_test.go
@@ -5,8 +5,8 @@ import "fmt"
 func ExampleSequence() {
 	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
+	ch := make(chan int)
 	for _, c := range numbers {
-		ch := make(chan int)
 		go func() {
 			fmt.Println(<-ch)
 		}()
