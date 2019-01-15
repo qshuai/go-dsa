cf43e1ec3f2e347b2bd96975f4e2a605da45f7b3
qshuai <qishuai231@gmail.com>
2019-01-15 22:02:49 +0800

    Add test case and go fmt

--- for-offer/find_duplication_element_test.go
+++ for-offer/find_duplication_element_test.go
@@ -3,30 +3,35 @@ package for_offer
 import "testing"
 
 func TestFindDuplicationElement(t *testing.T) {
-	tests := []struct{
-		name string
-		array []int
+	tests := []struct {
+		name   string
+		array  []int
 		target int
 	}{
 		{
-			name:"sorted array without duplicated element",
-			array:[]int{0, 1, 2, 3, 4, 5},
-			target:-1,
+			name:   "sorted array without duplicated element",
+			array:  []int{0, 1, 2, 3, 4, 5},
+			target: -1,
 		},
 		{
-			name:"shuffle array without duplicated element",
-			array:[]int{3,5,0,2,1,4},
-			target:-1,
+			name:   "shuffle array without duplicated element",
+			array:  []int{3, 5, 0, 2, 1, 4},
+			target: -1,
 		},
 		{
-			name:"duplicated element at index 0",
-			array:[]int{2, 0, 5, 2,1,3},
-			target:2,
+			name:   "duplicated element at index 0",
+			array:  []int{2, 0, 5, 2, 1, 3},
+			target: 2,
 		},
 		{
-			name:"duplicated element at arbitrary index",
-			array:[]int{3, 4,0, 1,5, 4},
-			target:4,
+			name:   "duplicated element at arbitrary index",
+			array:  []int{3, 4, 0, 1, 5, 4},
+			target: 4,
+		},
+		{
+			name:   "two duplicated elements",
+			array:  []int{3, 4, 0, 5, 5, 4},
+			target: 5,
 		},
 	}
 
