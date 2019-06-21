97c7fbca25b064b97091fff9a27a1f15f8ff417a
shuai.qi <shuai.qi@okcoin.com>
2019-06-21 11:51:58 +0800

    Java implement: alternative print odd and even number

--- golang/alternation_print_lddeven_test.go
+++ golang/alternation_print_lddeven_test.go
@@ -61,3 +61,59 @@ func ExamplePrintOddEvenAlternately() {
 	// goroutine 1: 9
 	// goroutine 2: 10
 }
+
+// java implement
+// import java.util.concurrent.locks.Lock;
+// import java.util.concurrent.locks.ReentrantLock;
+//
+// public class AlternativePrintOddEvenNumber {
+//     private static volatile int num = 1;
+//
+//     private static Lock lock = new ReentrantLock();
+//
+//     static class OddNum implements Runnable {
+//         public void run() {
+//             while (true) {
+//                 lock.lock();
+//                 if (num > 10) {
+//                     lock.unlock();
+//                     break;
+//                 }
+//
+//                 if (num % 2 != 0) {
+//                     System.out.printf("%s: %d\n",Thread.currentThread().getName(), num++);
+//                 }
+//                 lock.unlock();
+//             }
+//         }
+//     }
+//
+//     static class EvenNum implements Runnable {
+//         public void run() {
+//             while (true) {
+//                 lock.lock();
+//                 if (num > 10) {
+//                     lock.unlock();
+//                     break;
+//                 }
+//
+//                 if (num % 2 == 0) {
+//                     System.out.printf("%s: %d\n",Thread.currentThread().getName(), num++);
+//                 }
+//                 lock.unlock();
+//             }
+//         }
+//     }
+//
+//     public static void main(String[] args) {
+//         AlternativePrintOddEvenNumber r = new AlternativePrintOddEvenNumber();
+//         Thread oddThread = new Thread(new OddNum());
+//         oddThread.setName("thread-1");
+//
+//         Thread evenThread = new Thread(new EvenNum());
+//         evenThread.setName("thread-2");
+//
+//         oddThread.start();
+//         evenThread.start();
+//     }
+// }
