f564beb9508d734bd255cf2cbd218f32e53ac419
qishuai1 <qishuai1@guazi.com>
2022-03-30 10:03:32 +0800

    more effective algorithm for palindrome

--- leetcode/125Valid_Palindrome.go
+++ leetcode/125Valid_Palindrome.go
@@ -7,16 +7,72 @@ import (
 	"github.com/qshuai/go-dsa/types"
 )
 
+// https://leetcode.com/problems/valid-palindrome/
+
 const diffAlpha byte = 'a' - 'A'
 
 // isPalindrome 判断字符串是否为回文串
-// https://leetcode.com/problems/valid-palindrome/
+// solution 1：使用头尾指针实现
+// Best practice
 func isPalindrome(s string) bool {
 	if len(s) == 0 {
 		return true
 	}
 
-	l, size, middleNode := builDListNodes(s)
+	i, j := 0, len(s) -1
+	for j > i {
+		// 从前往后寻找alphanumeric
+		var a byte
+		for i <= j {
+			ok, isUpper := isAlphanumeric(s[i])
+			if !ok {
+				i++
+				continue
+			}
+
+			if isUpper {
+				a = s[i] + diffAlpha
+			} else {
+				a = s[i]
+			}
+			break
+		}
+
+		// 从后往前寻找alphanumeric
+		var b byte
+		for i <= j {
+			ok, isUpper := isAlphanumeric(s[j])
+			if !ok {
+				j--
+				continue
+			}
+
+			if isUpper {
+				b = s[j] + diffAlpha
+			} else {
+				b = s[j]
+			}
+			break
+		}
+
+		if a != b {
+			return false
+		}
+
+		i++
+		j--
+	}
+
+	return true
+}
+
+// solution 2: 使用链表实现
+func isPalindrome2(s string) bool {
+	if len(s) == 0 {
+		return true
+	}
+
+	l, size, middleNode := buildListNodes(s)
 	if l == nil {
 		return true
 	}
@@ -52,12 +108,12 @@ func isPalindrome(s string) bool {
 	return true
 }
 
-// builDListNodes 构建双向链表，并返回基本信息
+// buildListNodes 构建双向链表，并返回基本信息
 // returns:
 // 1. 链表头结点
 // 2. 元素数量size
 // 3. 中间节点元素(位置为 size / 2，结果四舍五入)
-func builDListNodes(s string) (*types.DListNode, int, *types.DListNode) {
+func buildListNodes(s string) (*types.DListNode, int, *types.DListNode) {
 	var l *types.DListNode
 
 	// 将字符串转换成byte数组
@@ -111,11 +167,27 @@ func builDListNodes(s string) (*types.DListNode, int, *types.DListNode) {
 	return l, size, middleNode
 }
 
-// isAlphanumeric 判断直接类型
+// isAlphanumeric 判断字节类型
 // return:
 // 1. 是否为字母数字类型的直接
 // 2. 判断是否为大写字母
 func isAlphanumeric(b byte) (bool, bool) {
+	if (b >= '0' && b <= '9') || (b >= 'a' && b <= 'z') {
+		return true, false
+	}
+
+	if b >= 'A' && b <= 'Z' {
+		return true, true
+	}
+
+	return false, false
+}
+
+// isAlphanumericRune 判断字符类型
+// return:
+// 1. 是否为字母数字类型的直接
+// 2. 判断是否为大写字母
+func isAlphanumericRune(b rune) (bool, bool) {
 	if (b >= '0' && b <= '9') || (b >= 'a' && b <= 'z') {
 		return true, false
 	}
--- leetcode/125Valid_Palindrome_test.go
+++ leetcode/125Valid_Palindrome_test.go
@@ -39,7 +39,11 @@ func Test_isPalindrome(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := isPalindrome(tt.args); got != tt.want {
-				t.Errorf("case: %s got %v, but want %v", tt.args, got, tt.want)
+				t.Errorf("method: isPalindrome, case: %s got %v, but want %v", tt.args, got, tt.want)
+			}
+
+			if got := isPalindrome2(tt.args); got != tt.want {
+				t.Errorf("method: isPalindrome2, case: %s got %v, but want %v", tt.args, got, tt.want)
 			}
 		})
 	}
