4aadcf1c2b2b04e5912675313c8062cf3e438752
qishuai1 <qishuai1@guazi.com>
2022-03-31 11:03:49 +0800

    optimise performance

--- leetcode/141Linked_List_Cycle.go
+++ leetcode/141Linked_List_Cycle.go
@@ -7,12 +7,17 @@ import (
 // https://leetcode.com/problems/linked-list-cycle/
 
 // hasCycle 检查单链表是否存在环
+// 使用快慢指针遍历链表，快指针每次走两步，慢指针每次走一步；
+// 边界条件：1. 慢指针在快指针的前面 2. 慢指针和快指针指向了同一个元素
 func hasCycle(head *types.ListNode) bool {
 	slow, fast := head, head
 	for fast != nil && fast.Next != nil {
+		if fast.Next == slow {
+			return true
+		}
+
 		slow = slow.Next
 		fast = fast.Next.Next
-
 		if slow == fast {
 			return true
 		}
