342d4407804f34573cdf2c06bbd326934b709a63
qishuai1 <qishuai1@guazi.com>
2022-04-07 15:33:46 +0800

    letter combine

---
+++ leetcode/17Letter_Combinations_of_a_Phone_Number.go
@@ -0,0 +1,35 @@
+package leetcode
+
+// https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+
+var letterMapping = [10][]string{nil, nil, {"a", "b", "c"}, {"d", "e", "f"}, {"g", "h", "i"},
+	{"j", "k", "l"}, {"m", "n", "o"}, {"p", "q", "r", "s"}, {"t", "u", "v"}, {"w", "x", "y", "z"}}
+
+const baseDigit byte = '0'
+
+// letterCombinations Given a string containing digits from 2-9 inclusive, return all possible letter combinations
+// that the number could represent. Return the answer in any order.
+// A mapping of digit to letters (just like on the telephone buttons) is given below.
+// Note that 1 does not map to any letters.
+func letterCombinations(digits string) []string {
+	if len(digits) <= 0 {
+		return []string{}
+	}
+
+	ret := make([]string, 0)
+	combine(digits, 0, "", &ret)
+
+	return ret
+}
+
+func combine(data string, idx int, middle string, collector *[]string) {
+	if len(middle) >= len(data) {
+		*collector = append(*collector, middle)
+		return
+	}
+
+	bs := letterMapping[data[idx]-baseDigit]
+	for i := 0; i < len(bs); i++ {
+		combine(data, idx+1, middle+bs[i], collector)
+	}
+}
---
+++ leetcode/17Letter_Combinations_of_a_Phone_Number_test.go
@@ -0,0 +1,27 @@
+package leetcode
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_letterCombinations(t *testing.T) {
+	tests := []struct {
+		name string
+		args string
+		want []string
+	}{
+		{
+			name: "two letters",
+			args: "23",
+			want: []string{"ad","ae","af","bd","be","bf","cd","ce","cf"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := letterCombinations(tt.args); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("letterCombinations() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
