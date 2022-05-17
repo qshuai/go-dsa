29d11bdfb15b6f44ce121e003733cdb77c32840f
qishuai1 <qishuai1@guazi.com>
2022-05-17 21:53:12 +0800

    quick sort

--- sort/quick_sort.go
+++ sort/quick_sort.go
@@ -2,20 +2,61 @@ package sort
 
 // QuickSort 快速排序，原地排序算法，不稳定的排序算法
 // 时间复杂度为nlogn（最坏的时间复杂度为n^2，取决于pivot的选取是否尽可能将数组平分）
-func QuickSort(arr []int) {
+func QuickSort(arr []int, fn func([]int) int) {
 	if len(arr) <= 1 {
 		return
 	}
 
-	pivot := partition(arr)
-	QuickSort(arr[:pivot])
-	QuickSort(arr[pivot+1:])
+	pivot := fn(arr)
+	QuickSort(arr[:pivot], fn)
+	QuickSort(arr[pivot+1:], fn)
 }
 
-func partition(arr []int) int {
+// PartitionHead 选择第一个元素作为pivot，进行分区
+func PartitionHead(arr []int) int {
+	pivot := arr[0]
+	i, j := len(arr)-1, len(arr)-1
+	for ; j > 0; j-- {
+		if arr[j] <= pivot {
+			continue
+		}
+
+		arr[i], arr[j] = arr[j], arr[i]
+		i--
+	}
+
+	arr[i], arr[0] = arr[0], arr[i]
+	return i
+}
+
+// PartitionMiddle 选择中间位置的元素作为pivot，进行分区
+func PartitionMiddle(arr []int) int {
+	idx := len(arr) / 2
+	pivot := arr[idx]
+	var i int
+	for j := 0; j < len(arr); j++ {
+		if arr[j] >= pivot {
+			continue
+		}
+
+		// track pivot element's index
+		if arr[i] == pivot {
+			idx = j
+		}
+
+		arr[i], arr[j] = arr[j], arr[i]
+		i++
+	}
+
+	arr[i], arr[idx] = arr[idx], arr[i]
+	return i
+}
+
+// PartitionTail 选择最后一个元素作为pivot，进行分区
+func PartitionTail(arr []int) int {
 	pivot := arr[len(arr)-1]
 	var i int
-	for j := 0; j < len(arr) - 1; j++ {
+	for j := 0; j < len(arr)-1; j++ {
 		if arr[j] >= pivot {
 			continue
 		}
@@ -26,4 +67,26 @@ func partition(arr []int) int {
 
 	arr[i], arr[len(arr)-1] = arr[len(arr)-1], arr[i]
 	return i
+}
+
+// PartitionMedian 三数取中法
+func PartitionMedian(arr []int) int {
+	begin, middle, end := arr[0], arr[len(arr)/2], arr[len(arr)-1]
+	if begin < middle {
+		if end < begin {
+			return PartitionHead(arr)
+		} else if middle < end {
+			return PartitionMiddle(arr)
+		} else {
+			return PartitionTail(arr)
+		}
+	} else {
+		if begin < end {
+			return PartitionHead(arr)
+		} else if middle > end {
+			return PartitionMiddle(arr)
+		} else {
+			return PartitionTail(arr)
+		}
+	}
 }
--- sort/quick_sort_test.go
+++ sort/quick_sort_test.go
@@ -5,24 +5,68 @@ import (
 	"testing"
 )
 
-func Test_QuickSort(t *testing.T) {
+func TestQuickSort(t *testing.T) {
 	tests := []struct {
-		name   string
-		array  []int
-		sorted []int
+		name     string
+		args     []int
+		copy     []int
+		expected []int
 	}{
 		{
-			name:   "standard example",
-			array:  []int{50, 10, 90, 30, 70, 40, 80, 60, 20},
-			sorted: []int{10, 20, 30, 40, 50, 60, 70, 80, 90},
+			name:     "case1(has sorted array)",
+			args:     []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+			copy:     []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+			expected: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+		},
+		{
+			name:     "case2(reversed array)",
+			args:     []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1},
+			copy:     []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1},
+			expected: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+		},
+		{
+			name:     "case3(truffle array)",
+			args:     []int{2, 3, 1, 345, 2, 56, 86, 34, 0, -23},
+			copy:     []int{2, 3, 1, 345, 2, 56, 86, 34, 0, -23},
+			expected: []int{-23, 0, 1, 2, 2, 3, 34, 56, 86, 345},
+		},
+		{
+			name:     "case4(one step sort)",
+			args:     []int{2, 1, 3, 4, 5, 6, 7, 8, 9, 10},
+			copy:     []int{2, 1, 3, 4, 5, 6, 7, 8, 9, 10},
+			expected: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+		},
+		{
+			name:     "case5(normal case)",
+			args:     []int{50, 10, 90, 30, 70, 40, 80, 60, 20},
+			copy:     []int{50, 10, 90, 30, 70, 40, 80, 60, 20},
+			expected: []int{10, 20, 30, 40, 50, 60, 70, 80, 90},
 		},
 	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			QuickSort(tt.args, PartitionHead)
+			if !reflect.DeepEqual(tt.args, tt.expected) {
+				t.Errorf("%s[PartitionHead] expected: %v but got: %v", tt.name, tt.expected, tt.args)
+			}
+			copy(tt.args, tt.copy) // args recovery
 
-	for _, test := range tests {
-		QuickSort(test.array)
-		if !reflect.DeepEqual(test.array, test.sorted) {
-			t.Errorf("%s, want: %v, but got: %v",
-				test.name, test.sorted, test.array)
-		}
+			QuickSort(tt.args, PartitionMiddle)
+			if !reflect.DeepEqual(tt.args, tt.expected) {
+				t.Errorf("%s[PartitionMiddle] expected: %v but got: %v", tt.name, tt.expected, tt.args)
+			}
+			copy(tt.args, tt.copy) // args recovery
+
+			QuickSort(tt.args, PartitionTail)
+			if !reflect.DeepEqual(tt.args, tt.expected) {
+				t.Errorf("%s[PartitionTail] expected: %v but got: %v", tt.name, tt.expected, tt.args)
+			}
+			copy(tt.args, tt.copy) // args recovery
+
+			QuickSort(tt.args, PartitionMedian)
+			if !reflect.DeepEqual(tt.args, tt.expected) {
+				t.Errorf("%s[PartitionMedian] expected: %v but got: %v", tt.name, tt.expected, tt.args)
+			}
+		})
 	}
 }
