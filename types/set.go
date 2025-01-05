a8d309889156b76e105054413632731415a4a692
qshuai <qishuai231@gmail.com>
2025-01-05 11:05:12 +0800

    feat: Set add method Remove

--- types/set.go
+++ types/set.go
@@ -1,11 +1,21 @@
 package types
 
+// Set is implemented in Go, using the Go standard library’s map
+// Set is not concurrency safe
 type Set[T comparable] struct {
 	container map[T]struct{}
 }
 
-func (s *Set[T]) Add(ele T) {
-	s.container[ele] = struct{}{}
+func (s *Set[T]) Add(eles ...T) {
+	for _, ele := range eles {
+		s.container[ele] = struct{}{}
+	}
+}
+
+func (s *Set[T]) Remove(eles ...T) {
+	for _, ele := range eles {
+		delete(s.container, ele)
+	}
 }
 
 func (s *Set[T]) Has(ele T) bool {
--- types/set_test.go
+++ types/set_test.go
@@ -2,7 +2,7 @@ package types
 
 import "testing"
 
-func TestSet_AddHas(t *testing.T) {
+func TestSet(t *testing.T) {
 	set := NewSet[int]()
 	set.Add(1)
 
@@ -11,5 +11,41 @@ func TestSet_AddHas(t *testing.T) {
 	}
 	if set.Has(2) {
 		t.Fatalf("set should not have ele: 2")
+	}
+
+	set.Remove(1)
+	if set.Has(1) {
+		t.Fatalf("element should have been removed: 1")
+	}
+}
+
+func TestSetWithCapacity(t *testing.T) {
+	set := NewSetWithCapacity[int](3)
+	set.Add(1, 2, 3)
+
+	if !set.Has(1) {
+		t.Fatalf("set should have ele: 1")
+	}
+	if !set.Has(2) {
+		t.Fatalf("set should have ele: 2")
+	}
+	if !set.Has(3) {
+		t.Fatalf("set should have ele: 3")
+	}
+	if set.Has(4) {
+		t.Fatalf("set should not have ele: 4")
+	}
+
+	set.Add(4)
+	if !set.Has(4) {
+		t.Fatalf("set should have ele: 4")
+	}
+	if !set.Has(1) {
+		t.Fatalf("set should have ele: 1")
+	}
+
+	set.Remove(2)
+	if set.Has(2) {
+		t.Fatalf("element should have been removed: 2")
 	}
 }
