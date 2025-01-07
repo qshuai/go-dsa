e09add5b889c75e38f2cd30a82d741c49c5f91cf
qshuai <qishuai231@gmail.com>
2025-01-07 18:16:50 +0800

    leetcode: 134. gas station

---
+++ leetcode/134_gas_station_test.go
@@ -0,0 +1,59 @@
+package leetcode
+
+import "testing"
+
+func Test_canCompleteCircuit(t *testing.T) {
+	type args struct {
+		gas  []int
+		cost []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "case-1",
+			args: args{
+				gas:  []int{1, 2, 3, 4, 5},
+				cost: []int{3, 4, 5, 1, 2},
+			},
+			want: 3,
+		},
+		{
+			name: "case-2",
+			args: args{
+				gas:  []int{2, 3, 4},
+				cost: []int{3, 4, 3},
+			},
+			want: -1,
+		},
+		{
+			name: "case-3",
+			args: args{
+				gas:  []int{5, 8, 2, 8},
+				cost: []int{6, 5, 6, 6},
+			},
+			want: 3,
+		},
+		{
+			name: "case-4",
+			args: args{
+				gas:  []int{5, 1, 2, 3, 4},
+				cost: []int{4, 4, 1, 5, 1},
+			},
+			want: 4,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := canCompleteCircuit(tt.args.gas, tt.args.cost); got != tt.want {
+				t.Errorf("canCompleteCircuit() = %v, want %v", got, tt.want)
+			}
+
+			if got := canCompleteCircuit2(tt.args.gas, tt.args.cost); got != tt.want {
+				t.Errorf("canCompleteCircuit() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
